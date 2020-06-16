bd23ff5df0444ab5e4ae09e347698902c45023e6
cleiton7 <cleiton.souza04@gmail.com>
2020-06-16 17:22:31 -0300

    Conectar API Spring no frontend

--- backend/sustentaMais/src/main/java/generation/sustentaMais/controller/UsuarioController.java
+++ backend/sustentaMais/src/main/java/generation/sustentaMais/controller/UsuarioController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/lista-usuarios")
+@RequestMapping("/usuario")
 @CrossOrigin("*")
 public class UsuarioController {
 	
--- sustenta-mais/src/app/service/usuarios.service.ts
+++ sustenta-mais/src/app/service/usuarios.service.ts
@@ -10,24 +10,22 @@ export class UsuariosService {
   constructor(private http: HttpClient) { }
 
   getAllUsuarios() {
-    return this.http.get('http://93.188.161.223:9000/user')
+    return this.http.get('http://localhost:8080/usuario')
   }
 
   postUsuario(usuario: Usuarios) {
-    return this.http.post('http://93.188.161.223:9000/user', usuario)
+    return this.http.post('http://localhost:8080/usuario', usuario)
   }
 
-
   putUsuario(usuario: Usuarios) {
-    return this.http.put('http://93.188.161.223:9000/user', usuario)
+    return this.http.put('http://localhost:8080/usuario', usuario)
   }
 
   getByIdUsuario(id: number){
-    return this.http.get(`http://93.188.161.223:9000/user/${id}`)
+    return this.http.get(`http://localhost:8080/usuario/${id}`)
   }
 
   deleteUsuario(id: number) {
-    return this.http.delete(`http://93.188.161.223:9000/user/${id}`)
+    return this.http.delete(`http://localhost:8080/usuario/${id}`)
   }
-
 }
