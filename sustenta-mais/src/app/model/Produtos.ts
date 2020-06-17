33aeaaa9f05fb9a89b37f9dc3d8f097dc4a260f7
Hugo Brito <hgbrito.tr@gmail.com>
2020-06-17 16:24:09 -0300

    Criação no front da model e do service de produtos

---
+++ sustenta-mais/src/app/model/Produtos.ts
@@ -0,0 +1,9 @@
+export class Produtos {
+    public id: number
+    public nome: string
+    public descricao: string
+    public preco: number
+    public imagem: string
+    public quantidade: number
+}
+
---
+++ sustenta-mais/src/app/service/produtos.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Produtos } from '../model/Produtos';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProdutosService {
+
+  constructor(private http: HttpClient) { }
+
+  getAllProdutos(page: number, size: number) {
+    return this.http.get('http://localhost:8080/produtos/${page}/${size}')
+  }
+
+  getByIdProduto(id: number) {
+    return this.http.get(`http://localhost:8080/produtos/${id}`)
+  }
+
+  getByNameProduto(nome: string) {
+    return this.http.get(`http://localhost:8080/produtos/nome/${nome}`)
+  }
+
+  postProduto(produto: Produtos) {
+    return this.http.post('http://localhost:8080/produtos', produto)
+  }
+
+  putProduto(produto: Produtos) {
+    return this.http.put('http://localhost:8080/produtos', produto)
+  }
+
+  deleteProduto(id: number) {
+    return this.http.delete(`http://localhost:8080/produtos/${id}`)
+  }
+}
